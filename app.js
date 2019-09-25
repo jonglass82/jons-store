d21c5d48f6bef116b91ea67337c47ac746322b55
Jon Glass <jonglass82@gmail.com>
2019-09-25 16:09:11 -0500

    set up initial mongodb installations

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:3001';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// MongoClient.connect(url, function(err,db){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//   console.log('database connected!');
+//   db.close();
+//   }
+// });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*"); 
+  next(); 
+});
+
+app.get('/api/products', (req, res) => res.json(['blue shirt', "yellow shirt"]));
+
+app.get('/api/product', function (req, res) {
+  res.send("here is the product you wanted to see!");
+  console.log("received your get request!");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
