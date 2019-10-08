823b435371c6ece127e79263e2500ed949a83922
Jon Glass <jonglass82@gmail.com>
2019-10-07 20:51:29 -0500

    complete mongoDb integration

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:3001';
+const url = `mongodb+srv://jonglass:${process.env.MONGO_DB_PASSWORD}@cluster0-w4qcc.mongodb.net/jonsStore?retryWrites=true&w=majority`;
 
-// MongoClient.connect(url, function(err,db){
-//   if(err){
-//     console.log(err);
-//   }
-//   else{
-//   console.log('database connected!');
-//   db.close();
-//   }
-// });
+let mongoDb;
+
+MongoClient.connect(url, function(err,db){
+  if (err){
+    console.log(err);
+  } else {
+  console.log('database connected!');
+  mongoDb = db.db('jonsStore');
+  // db.close();
+  }
+});
 
 app.use((request, response, next) => {
   response.header("Access-Control-Allow-Origin", "*"); 
   next(); 
 });
 
-app.get('/api/products', (req, res) => res.json(['blue shirt', "yellow shirt"]));
+app.get('/api/products', async (req,res) => {
+  const products = await mongoDb.collection('products').find().toArray();
+  console.log(products)
+  res.json(products)
+});
+
 
 app.get('/api/product', function (req, res) {
   res.send("here is the product you wanted to see!");
