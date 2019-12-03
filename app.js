5a212bbbd6e030e217b13341bbfa47a47d029782
Jon Glass <jonglass82@gmail.com>
2019-12-02 19:34:55 -0600

    add images array to new products

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 const app = express();
+const cloudinary = require('cloudinary').v2;
 const jwt = require('jsonwebtoken');
 const bodyParser  = require('body-parser');
 const ObjectId = require('mongodb').ObjectID;
@@ -9,10 +10,14 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const url = `mongodb+srv://jonglass:${process.env.MONGO_DB_PASSWORD}@cluster0-w4qcc.mongodb.net/jonsStore?retryWrites=true&w=majority`;
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+cloudinary.config({ 
+  cloud_name: '${process.env.CLOUD_NAME}', 
+  api_key: '${process.env.CLOUDINARY_KEY}', 
+  api_secret: '${process.env.CLOUDINARY_SECRET}' 
+});
 
 let mongoDb;
 
@@ -35,18 +40,27 @@ app.get('/api/products', async (req,res) => {
 });
 
 
-
 app.post('/api/create', cors(), async (req,res) => {
-  console.log(req.body);
 
   const legit = jwt.verify(req.body.auth, process.env.SECRET_KEY);
 
   if (legit){
 
-  const products = await mongoDb.collection('products');
+      const products = await mongoDb.collection('products');
 
-  products.insertOne({title: req.body.title, description: req.body.description, price: req.body.price});
-  }
+      products.insertOne({
+        title: req.body.title, 
+        description: req.body.description, 
+        price: req.body.price,
+        shipping: req.body.shipping,
+        sold: req.body.sold,
+        available: req.body.available,
+        category: req.body.category, 
+        images: req.body.images
+      });
+      console.log("your product has been created!")
+    }
+
   else{
     res.send("You do not have access!!!")
   }
@@ -89,11 +103,10 @@ app.put('/api/update/:id', async (req,res) => {
         price: req.body.newPrice 
       } }
     );
-
-    console.log(req.body)
+    res.send("Your product has been updated!");
   }
   else{
-    console.log("no product found ... ")
+    res.send("no product found ... ");
   }
 
 })
